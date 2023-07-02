4cac2bc5239b0af5cd3b90b90064398a51ea0ec9
Nazar Tsventarnyi <110458518+IloveKatyara1@users.noreply.github.com>
2023-07-02 09:08:08 +0300

    added adaptiveness on header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -10,20 +10,46 @@ import phone from '../../assets/icons/phone.svg';
 import scales from '../../assets/icons/scales.svg';
 import basket from '../../assets/icons/basket.svg';
 import heart from '../../assets/icons/heart.png';
+import logoWhite from '../../assets/img/white_logo.png';
+import phoneWhite from '../../assets/icons/white_phone.png';
+import scalesWhite from '../../assets/icons/white_scales.png';
+import basketWhite from '../../assets/icons/white_basket.png';
+import heartWhite from '../../assets/icons/white_heart.png';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header_top">
                 <div className="container">
+                    <div className="header_nav_right header_nav_right_mobile">
+                        <img className="header_nav_right__img" src={log} alt="" />
+                        <div className="header_nav_right_text">
+                            <h3 className="header_nav_right_sign">Вхід</h3>
+                            <hr className="header_nav__hr" />
+                            <h3 className="header_nav_right_sign">Реестрація</h3>
+                        </div>
+                    </div>
                     <nav className="header_nav">
                         <ul className="header_nav__ul">
-                            <li className="header__li arrow_dropdown">Про нас</li>
-                            <li className="header__li">Каталог продукції</li>
-                            <li className="header__li">Оплата і доставка</li>
-                            <li className="header__li">Партнери</li>
-                            <li className="header__li">Новини</li>
-                            <li className="header__li">Контакти</li>
+                            <hr className="header_nav__hr_mobile" />
+                            <a href="#">
+                                <li className="header__li arrow_dropdown">Про нас</li>
+                            </a>
+                            <a href="#">
+                                <li className="header__li">Каталог продукції</li>
+                            </a>
+                            <a href="#">
+                                <li className="header__li">Оплата і доставка</li>
+                            </a>
+                            <a href="#">
+                                <li className="header__li">Партнери</li>
+                            </a>
+                            <a href="#">
+                                <li className="header__li">Новини</li>
+                            </a>
+                            <a href="#">
+                                <li className="header__li">Контакти</li>
+                            </a>
                         </ul>
                         <hr className="header_nav__hr" />
                         <div className="header_nav_right">
@@ -35,47 +61,78 @@ const Header = () => {
                             </div>
                         </div>
                     </nav>
+                    <div className="header__hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
                 </div>
             </div>
             <div className="header_midle">
                 <div className="container">
                     <div className="header_midle_content">
-                        <img src={logo} alt="DAT" />
-                        <div className="header_search">
-                            <input type="text" className="header_search__input" placeholder="Пошук" />
-                            <hr className="header_search__hr" />
-                            <img src={iconSearch} alt="search" className="header_search__icon" />
-                        </div>
-                        <div className="header_support">
-                            <button className="button_circle">
-                                <img src={phone} alt="phone" />
-                            </button>
-                            <div className="header_support_wrapper">
-                                <a href="tel:+380671150058" className="header_support__phone arrow_dropdown">
-                                    +38 (067) 115 00 58
-                                </a>
-                                <a href="" className="header_support__descr">
-                                    Замовити зворотній зв’язок
-                                </a>
+                        <div className="header_midle_left">
+                            <img src={logo} alt="DAT" className="header__logo" />
+                            <div className="header_search">
+                                <input type="text" className="header_search__input" placeholder="Пошук" />
+                                <hr className="header_search__hr" />
+                                <img src={iconSearch} alt="search" className="header_search__icon" />
                             </div>
                         </div>
-                        <div className="header_circles">
-                            <button className="button_circle">
-                                <img src={heart} alt="вподобані" />
-                            </button>
-                            <button className="button_circle">
-                                <img src={scales} alt="ваги" />
-                            </button>
-                            <button className="button_circle">
-                                <img src={basket} alt="корзина" />
-                            </button>
+                        <div className="header_midle_right">
+                            <div className="header_support">
+                                <button className="button_circle">
+                                    <img src={phone} alt="phone" />
+                                </button>
+                                <div className="header_support_wrapper">
+                                    <a href="tel:+380671150058" className="header_support__phone arrow_dropdown">
+                                        +38 (067) 115 00 58
+                                    </a>
+                                    <a href="#" className="header_support__descr">
+                                        Замовити зворотній зв’язок
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="header_circles">
+                                <button className="button_circle">
+                                    <img src={heart} alt="вподобані" />
+                                </button>
+                                <button className="button_circle">
+                                    <img src={scales} alt="ваги" />
+                                </button>
+                                <button className="button_circle">
+                                    <img src={basket} alt="корзина" />
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="header_bottom">
-                <div className="container">
+                <div className="container header_bottom_container">
+                    <button className="button_header button_header_active">
+                        <img src={iconBtn1} alt="" />
+                        <p className="arrow_dropdown">Насіння</p>
+                    </button>
                     <button className="button_header">
+                        <img src={iconBtn2} alt="" />
+                        <p className="maxWidth">Засоби захисту рослин</p>
+                    </button>
+                    <button className="button_header">
+                        <img src={iconBtn3} alt="" />
+                        <p>Добрива</p>
+                    </button>
+                    <button className="button_header">
+                        <img src={iconBtn4} alt="" />
+                        <p>кормова група</p>
+                    </button>
+                    <button className="button_header">
+                        <img src={iconBtn5} alt="" />
+                        <p>Агроному в поміч</p>
+                    </button>
+                </div>
+                <div className="container header_bottom_container_mobile">
+                    <button className="button_header button_header_active arrow_dropdown">
                         <img src={iconBtn1} alt="" />
                         <p>Насіння</p>
                     </button>
@@ -96,6 +153,61 @@ const Header = () => {
                         <p>Агроному в поміч</p>
                     </button>
                 </div>
+            </div>
+            <div className="header_sidebar">
+                <div className="header_sidebar_top">
+                    <img src={logoWhite} alt="white logo" className="header__logo" />
+                    <div className="header_circles">
+                        <button className="button_circle button_circle_white">
+                            <img src={heartWhite} alt="вподобані" />
+                        </button>
+                        <button className="button_circle button_circle_white">
+                            <img src={scalesWhite} alt="ваги" />
+                        </button>
+                        <button className="button_circle button_circle_white">
+                            <img src={basketWhite} alt="корзина" />
+                        </button>
+                    </div>
+                </div>
+                <div className="header_search">
+                    <input type="text" className="header_search__input" placeholder="Пошук" />
+                    <hr className="header_search__hr" />
+                    <img src={iconSearch} alt="search" className="header_search__icon" />
+                </div>
+                <div className="header_support">
+                    <button className="button_circle button_circle_white">
+                        <img src={phoneWhite} alt="phone" />
+                    </button>
+                    <div className="header_support_wrapper">
+                        <a href="tel:+380671150058" className="header_support__phone arrow_dropdown">
+                            +38 (067) 115 00 58
+                        </a>
+                        <a href="#" className="header_support__descr">
+                            Замовити зворотній зв’язок
+                        </a>
+                    </div>
+                </div>
+                <ul className="header_sidebar__ul">
+                    <hr className="header_sidebar__hr" />
+                    <a href="#">
+                        <li className="header__li">Про компанію</li>
+                    </a>
+                    <a href="#">
+                        <li className="header__li">Каталог продукції</li>
+                    </a>
+                    <a href="#">
+                        <li className="header__li">Оплата і доставка</li>
+                    </a>
+                    <a href="#">
+                        <li className="header__li">Партнери</li>
+                    </a>
+                    <a href="#">
+                        <li className="header__li">Новини</li>
+                    </a>
+                    <a href="#">
+                        <li className="header__li">Контакти</li>
+                    </a>
+                </ul>
             </div>
         </header>
     );
